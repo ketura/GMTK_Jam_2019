aa49bfd7c9cc63248bdfd7a63ff9561a73503ffc
Solonarv <nstamm@gmx.de>
2019-08-03 17:35:55 +0200

    Test scene for split/merge behavior.
    Press S to split a random blob
    Press M to merge two random blobs

---
+++ Assets/Scenes/split_test/SplitController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            SplitRandomBlob();
+        }
+
+        if (Input.GetKeyDown("m"))
+        {
+            MergeRandomBlobs();
+        }
+    }
+
+    void SplitRandomBlob()
+    {
+        GameObject[] blobs = GameObject.FindGameObjectsWithTag("Blob");
+        if (blobs.Length > 0)
+        {
+            blobs[Random.Range(0, blobs.Length)].GetComponent<Splitting>().Split();
+        }
+    }
+
+    void MergeRandomBlobs()
+    {
+        GameObject[] blobs = GameObject.FindGameObjectsWithTag("Blob");
+        if (blobs.Length < 2) return;
+        int i = Random.Range(1, blobs.Length);
+        int j = Random.Range(0, i);
+        blobs[i].GetComponent<Splitting>().Merge(blobs[j]);
+    }
+}
