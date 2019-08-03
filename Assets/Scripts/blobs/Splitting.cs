41fe1f20d38f4889adc029c5febf2417856ee7b2
Solonarv <nstamm@gmx.de>
2019-08-03 19:20:42 +0200

    Splitting now conserves momentum
    Combining blobs add their momenta, splitting blobs just pass
    down their velocity to the children.
    Also, when splitting blobs they are now separated.

--- Assets/Scripts/blobs/Splitting.cs
+++ Assets/Scripts/blobs/Splitting.cs
@@ -7,14 +7,15 @@ using UnityEngine;
 public class Splitting : MonoBehaviour
 {
 
-    private Life LifePool;
+    private Life lifePool;
     public int StartingHP;
+    private double scaleFactor;
 
     // Start is called before the first frame update
     void Start()
     {
-        LifePool = GetComponent<Life>();
-        ScaleVolume();
+        lifePool = GetComponent<Life>();
+        ScaleVolumeAndMass();
     }
 
     // Update is called once per frame
@@ -26,35 +27,65 @@ public class Splitting : MonoBehaviour
     // Split the attached GameObject into two. Life is split evenly,
     // everything else is copied.
     public void Split()
-    {        
+    {
+
+        Vector3 offset = VectorUtils.RandomHorizontalUnitVector() * (float)scaleFactor/2;
+
         GameObject child1 = Instantiate(gameObject);
-        child1.GetComponent<Life>().MaxHP = LifePool.MaxHP / 2;
-        child1.GetComponent<Life>().HP = LifePool.HP / 2;
+        child1.GetComponent<Life>().MaxHP = lifePool.MaxHP / 2;
+        child1.GetComponent<Life>().HP = lifePool.HP / 2;
+        child1.GetComponent<Transform>().position += offset;
 
         GameObject child2 = Instantiate(gameObject);
-        child2.GetComponent<Life>().MaxHP = (LifePool.MaxHP + 1) / 2;
-        child2.GetComponent<Life>().HP = (LifePool.HP + 1) / 2;
+        child2.GetComponent<Life>().MaxHP = (lifePool.MaxHP + 1) / 2;
+        child2.GetComponent<Life>().HP = (lifePool.HP + 1) / 2;
+        child2.GetComponent<Transform>().position -= offset;
+
 
         Destroy(gameObject);
     }
 
-    // Combine the attached GameObject with another one. Life is combined.
+    // Combine the attached GameObject with another one. Life and momenta are combined.
     public void Combine(GameObject other)
     {
         // Actually just "eats" the other object
         Life otherLife = other.GetComponent<Life>();
         if (otherLife == null) return;
 
-        LifePool.MaxHP += otherLife.MaxHP;
-        LifePool.HP += otherLife.HP;
-        ScaleVolume();
+        GameObject combined = Instantiate(gameObject);
+
+        // Combine life totals
+        Life combinedLife = combined.GetComponent<Life>();
+        combinedLife.MaxHP = lifePool.MaxHP + otherLife.MaxHP;
+        combinedLife.HP = lifePool.HP + otherLife.HP;
+        combined.GetComponent<Splitting>().Start();
+
+        // Combine momenta
+        Rigidbody myBody = GetComponent<Rigidbody>();
+        Rigidbody otherBody = other.GetComponent<Rigidbody>();
+        if (myBody != null && otherBody != null)
+        {
+            Vector3 totalMomentum = myBody.mass * myBody.velocity + otherBody.mass * otherBody.velocity;
+            Rigidbody combinedBody = combined.GetComponent<Rigidbody>();
+            combinedBody.velocity = totalMomentum / combinedBody.mass;
+        }
+
+        // clean up
         Destroy(other);
+        Destroy(gameObject);
     }
 
-    private void ScaleVolume()
+    private void ScaleVolumeAndMass()
     {
         Transform transform = GetComponent<Transform>();
-        double scaleFactor = Math.Pow((double)LifePool.MaxHP / StartingHP, (double) 1 / 3);
+        double volFactor = (double)lifePool.MaxHP / StartingHP;
+        scaleFactor = Math.Pow(volFactor, (double) 1/3);
         transform.localScale = Vector3.one * (float) scaleFactor;
+
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            body.mass = (float)volFactor;
+        }
     }
 }
