53038e2717aa89ca50a472981c9fd3060d4688fc
Mink <alekseyminkin96@google.com>
2019-08-04 12:46:02 +0200

    First level walls, menus

---
+++ Assets/Scripts/UI/SceneSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoMainMenu
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GotoControlDescription()
+    {
+        SceneManager.LoadScene("ControlsExplanation");
+    }
+	
+	public void GotoBackstory()
+    {
+        SceneManager.LoadScene("Backstory");
+    }
+	
+	public void GotoFirstLevel()
+    {
+        SceneManager.LoadScene("Tutorial Level");
+    }
+	
+	public void ExitGame()
+    {
+        //SceneManager.LoadScene("Tutorial level");
+    }
+}
+
+
