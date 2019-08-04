78331add83dfa277c6231d04007940577c66b9dc
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2019-08-03 19:12:16 -0500

    preliminary work on selection box.

--- Assets/Scripts/blobs/SelectionController.cs
+++ Assets/Scripts/blobs/SelectionController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 using Utilities;
+//using RektTransform;
+
 
 public enum SelectionMode { Standard, Add, Remove, Toggle };
 
@@ -13,16 +15,25 @@ public class SelectionController : Singleton<SelectionController>
 
 	public LayerMask ClickableMask;
 
+	public RectTransform SelectionBox;
+
+	private Vector2 StartPos;
+	private Vector2 EndPos;
+
 	private bool LeftDown = false;
 	private bool RightDown = false;
 	private bool MidDown = false;
 
 	private UnitController UController;
 
+	private Camera UICamera;
+
 	// Start is called before the first frame update
 	void Start()
 	{
 		UController = UnitController.Instance;
+		SelectionBox.gameObject.SetActive(false);
+		UICamera = GameObject.FindGameObjectWithTag("UICamera").GetComponent<Camera>();
 	}
 
 	// Update is called once per frame
@@ -36,6 +47,10 @@ public class SelectionController : Singleton<SelectionController>
 		{
 			Debug.Log("Clicking");
 			RaycastHit rayhit;
+
+			//StartPos = UICamera.ScreenToViewportPoint(Input.mousePosition);
+			StartPos = Input.mousePosition;
+			Debug.Log(StartPos);
 
 			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayhit, Mathf.Infinity, ClickableMask))
 			{
@@ -64,15 +79,44 @@ public class SelectionController : Singleton<SelectionController>
 				else
 				{
 					UController.ClearSelection();
-					UController.AddUnitToSelection(unit);				}
+					UController.AddUnitToSelection(unit);
+				}
 				
-
+				
 			}
 
 		}
 		else if(Input.GetButton("Select"))
 		{
+			EndPos = UICamera.ScreenToViewportPoint(Input.mousePosition);
+			EndPos = Input.mousePosition;
+			Debug.Log(EndPos);
 
+			//var canvas = SelectionBox.GetRootCanvas().GetComponent<RectTransform>();
+
+			float width = Screen.width;
+			float height = Screen.height;
+
+			float x1 = StartPos.x;// * width;
+			float x2 = EndPos.x;// * width;
+			float y1 = StartPos.y;// * height;
+			float y2 = EndPos.y;// * height;
+
+			SelectionBox.gameObject.SetActive(true);
+
+			SelectionBox.sizeDelta = new Vector2(Mathf.Abs(x1 - x2), Mathf.Abs(y1 - y2));
+			//SelectionBox.
+
+			//SelectionBox.Left(Mathf.Min(x1, x2));
+			//SelectionBox.Right(Mathf.Max(x1, x2));
+			//SelectionBox.Bottom(Mathf.Min(y1, y2));
+			//SelectionBox.Top(Mathf.Max(y1, y2));
+
+			
+		}
+		else
+		{
+			SelectionBox.gameObject.SetActive(false);
 		}
 
 
